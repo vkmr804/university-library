d9981cea24f17c9fe3d74e8bc3281902da11eadd
Manaswini Chowdary <nxv53030@ucmo.edu>
2022-12-08 17:51:39 -0600

    added annotations

--- src/main/java/com/ucm/thesis/dto/TutorialUpdateRequest.java
+++ src/main/java/com/ucm/thesis/dto/TutorialUpdateRequest.java
@@ -1,12 +1,18 @@
 package com.ucm.thesis.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class TutorialUpdateRequest {
     private String title;
     private String description;
     private boolean published;
+    
+    
 }
