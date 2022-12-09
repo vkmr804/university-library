cf251d1a6e459b80af70cd7ba8fdc5f158290af7
Manaswini Chowdary <nxv53030@ucmo.edu>
2022-12-09 10:10:28 -0600

    arguments added

--- src/main/java/com/ucm/thesis/dto/AuthorRequest.java
+++ src/main/java/com/ucm/thesis/dto/AuthorRequest.java
@@ -1,10 +1,16 @@
 package com.ucm.thesis.dto;
 
+import com.ucm.thesis.dto.TutorialUpdateRequest.TutorialUpdateRequestBuilder;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class AuthorRequest {
     private String name;
 }
--- src/main/java/com/ucm/thesis/dto/TutorialRequest.java
+++ src/main/java/com/ucm/thesis/dto/TutorialRequest.java
@@ -1,10 +1,16 @@
 package com.ucm.thesis.dto;
 
+import com.ucm.thesis.dto.TutorialUpdateRequest.TutorialUpdateRequestBuilder;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class TutorialRequest {
     private String title;
     private String description;
