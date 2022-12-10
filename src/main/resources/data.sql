41437e9ced76acb9a6e998282699cca4f9bcc70d
Manaswini Chowdary <nxv53030@ucmo.edu>
2022-12-09 19:37:00 -0600

    added db fields

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,10 +3,10 @@ INSERT INTO AUTHOR (id, name) VALUES (2, 'Prof. Javeria');
 INSERT INTO AUTHOR (id, name) VALUES (3, 'Prof. Kesh');
 INSERT INTO AUTHOR (id, name) VALUES (4, 'Prof. Ma');
 
-INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (1, 'UCM', 'Advanced Applications', 1, true);
-INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (2, 'Title 2', 'Test title for the Tutorial', 1, true);
-INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (3, 'Title 3', 'Test title for the Tutorial', 2, true);
-INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (4, 'Title 4', 'Test title for the Tutorial', 2, true);
-INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (5, 'Title 5', 'Test title for the Tutorial', 3, true);
-INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (6, 'Title 6', 'Test title for the Tutorial', 3, true);
-INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (7, 'Title 7', 'Test title for the Tutorial', 4, true);
+INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (1, 'Management information system', 'Advanced Applications', 1, true);
+INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (2, 'Big data architecture', 'Test title for the Tutorial', 1, true);
+INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (3, 'Big data solutions for business', 'Test title for the Tutorial', 2, true);
+INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (4, 'Business application machine deep learning', 'Test title for the Tutorial', 2, true);
+INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (5, 'Data resource management', 'Test title for the Tutorial', 3, true);
+INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (6, 'Info vis for info system', 'Test title for the Tutorial', 3, true);
+INSERT INTO TUTORIAL (id, title, description, author_id, published) VALUES (7, 'Project management', 'Test title for the Tutorial', 4, true);
